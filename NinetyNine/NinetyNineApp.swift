41e015bddfc9e937f72b00090da24d02dd7cf4ae
dejes <jack940197@gmail.com>
2021-03-10 16:20:18 +0800

    Initial Commit

---
+++ NinetyNine/NinetyNineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NinetyNineApp.swift
+//  NinetyNine
+//
+//  Created by User14 on 2021/3/10.
+//
+
+import SwiftUI
+
+@main
+struct NinetyNineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
